00d44fab52fa694075b7284302ded1e2d88c6b73
maaviah17 <muawiyah1705@gmail.com>
2026-04-12 13:22:41 +0530

    connecting with frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,13 @@ const app = express();
 const cookieParser = require("cookie-parser")
 const authRoutes = require("./routes/auth.route")
 const foodRoutes = require("./routes/food.route")
+const cors = require("cors")
+
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 
 app.use(express.json());
 app.use(cookieParser())
